cb33b59cd9a258e1568aa2184fafbee4f377ed94
matsalinas <matsalinas@uchile.cl>
2021-03-10 20:13:33 +0000

    Adding s3boto3 file storage

---
+++ openassessment/fileupload/tests/test_api_s3boto3.py
@@ -0,0 +1,96 @@
+import ddt
+
+from django.test import TestCase
+from django.test.utils import override_settings
+
+import boto3
+from moto import mock_s3
+from mock import patch
+from pytest import raises
+from openassessment.fileupload import api, exceptions
+
+
+@ddt.ddt
+@override_settings(
+    ORA2_FILEUPLOAD_BACKEND='s3boto3',
+)
+class TestFileUploadService(TestCase):
+    @mock_s3
+    @override_settings(
+        AWS_ACCESS_KEY_ID="foobar",
+        AWS_SECRET_ACCESS_KEY="bizbaz",
+        FILE_UPLOAD_STORAGE_BUCKET_NAME="mybucket",
+    )
+    def test_get_upload_url(self):
+        conn = boto3.client("s3")
+        conn.create_bucket(Bucket="mybucket")
+        uploadUrl = api.get_upload_url("foo", "bar")
+        self.assertIn("/submissions_attachments/foo", uploadUrl)
+
+    @mock_s3
+    @override_settings(
+        AWS_ACCESS_KEY_ID="foobar",
+        AWS_SECRET_ACCESS_KEY="bizbaz",
+        FILE_UPLOAD_STORAGE_BUCKET_NAME="mybucket",
+    )
+    def test_get_download_url(self):
+        conn = boto3.client("s3")
+        conn.create_bucket(Bucket="mybucket")
+        conn.put_object(
+            Bucket="mybucket",
+            Key="submissions_attachments/foo/content",
+            Body=b"How d'ya do?"
+        )
+        downloadUrl = api.get_download_url("foo")
+        self.assertIn("/submissions_attachments/foo/content", downloadUrl)
+
+    @mock_s3
+    @override_settings(
+        AWS_ACCESS_KEY_ID="foobar",
+        AWS_SECRET_ACCESS_KEY="bizbaz",
+        FILE_UPLOAD_STORAGE_BUCKET_NAME="mybucket",
+    )
+    def test_remove_file(self):
+        conn = boto3.client("s3")
+        conn.create_bucket(Bucket="mybucket")
+        conn.put_object(
+            Bucket="mybucket",
+            Key="submissions_attachments/foo/content",
+            Body=b"Test"
+        )
+        result = api.remove_file("foo")
+        self.assertTrue(result)
+        result = api.remove_file("foo")
+        self.assertFalse(result)
+
+    def test_get_upload_url_no_bucket(self):
+        with raises(exceptions.FileUploadInternalError):
+            api.get_upload_url("foo", "bar")
+
+    def test_get_upload_url_no_key(self):
+        with raises(exceptions.FileUploadRequestError):
+            api.get_upload_url("", "bar")
+
+    @mock_s3
+    @override_settings(
+        AWS_ACCESS_KEY_ID="foobar",
+        AWS_SECRET_ACCESS_KEY="bizbaz",
+        FILE_UPLOAD_STORAGE_BUCKET_NAME="mybucket",
+    )
+    @patch.object(boto3, "client")
+    def test_get_upload_url_error(self, mock_s3):
+        with raises(exceptions.FileUploadInternalError):
+            mock_s3.side_effect = Exception("Oh noes")
+            api.get_upload_url("foo", "bar")
+
+    @mock_s3
+    @override_settings(
+        AWS_ACCESS_KEY_ID="foobar",
+        AWS_SECRET_ACCESS_KEY="bizbaz",
+        FILE_UPLOAD_STORAGE_BUCKET_NAME="mybucket",
+    )
+    @patch.object(boto3, "client")
+    def test_get_download_url_error(self, mock_s3):
+        with raises(exceptions.FileUploadInternalError):
+            mock_s3.side_effect = Exception("Oh noes")
+            api.get_download_url("foo")
